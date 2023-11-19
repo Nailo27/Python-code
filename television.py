db79a2d48dddb9cd65935d869951a8211d5fa69a
Nailo27 <150855063+Nailo27@users.noreply.github.com>
2023-11-18 21:15:38 -0600

    Edited for correctness

--- television.py
+++ television.py
@@ -14,10 +14,17 @@ class Television:
         self.__status = not self.__status
 
     def mute(self):
+        """
+        When the Television is on, the mute status is changed.
+        """
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self):
+        """
+        When the power is on, the channel is increased by one.
+        if it is at max, go to min.
+        """
         if self.__status:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
@@ -25,6 +32,10 @@ class Television:
                 self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
+        """
+            While Television is on, decrease the channel by 1.
+             If at min, go to max.
+        """
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -32,10 +43,10 @@ class Television:
                 self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self):
-        # ***
-        # While Television is on, increase volume value by 1. If at maximum, stay at max.
-        # If muted, unmute then continue
-        # ***
+        """
+            While Television is on, increase volume by 1. If at max, stay.
+            If muted, unmute then continue.
+        """
         if self.__status:
             if self.__muted:
                 self.mute()
@@ -45,6 +56,10 @@ class Television:
                 self.__volume = Television.MAX_VOLUME
 
     def volume_down(self):
+        """
+            While Television is on, decrease volume by 1. If at min, stay.
+            If muted, unmute then continue.
+        """
         if self.__status:
             if self.__muted:
                 self.mute()
@@ -54,7 +69,7 @@ class Television:
                 self.__volume = Television.MIN_VOLUME
 
     def __str__(self):
-        if  self.__muted:
+        if self.__muted:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         else:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
